3c29590a4b0a6d2e337d89bc76b0e90f8615e9f6
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-04-09 21:10:30 +0300

    added enemy movement based on nav mesh

---
+++ InertialShooterUnity/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace InertialShooter.Enemies
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _agent;
+
+        private Transform _target;
+
+        private void Awake()
+        {
+            _agent.updateRotation = false;
+            _agent.updateUpAxis = false;
+        }
+
+        private void Start()
+        {
+            _agent.speed = Random.Range(1, 10);
+        }
+
+        private void Update()
+        {
+            if (_target != null)
+            {
+                _agent.SetDestination(_target.position);
+            }
+        }
+
+        public void Initialize(Transform target)
+        {
+            _target = target;
+        }
+    }
+}
---
+++ InertialShooterUnity/Assets/Scripts/Enemies/Spawner/EnemyWaveSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+namespace InertialShooter.Enemies.Spawner
+{
+    public class EnemyWaveSpawner : MonoBehaviour
+    {
+        [SerializeField] private Transform _playerTransform;
+        [SerializeField] private GameObject _enemy;
+
+        [Space(10)]
+        
+        [SerializeField] private Transform[] _spawningSpots;
+
+        [Space(10)]
+        
+        [Min(0.1f)] [SerializeField] private float _timeBetweenSpawn = 1;
+        
+        private void Start()
+        {
+            StartCoroutine(CreateEnemiesOnScene());
+        }
+        
+        private IEnumerator CreateEnemiesOnScene()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(_timeBetweenSpawn);
+
+                EnemyMovement enemyMove =
+                    Instantiate(_enemy, _spawningSpots[Random.Range(0, _spawningSpots.Length)].position,
+                        Quaternion.identity).GetComponent<EnemyMovement>();
+                enemyMove.Initialize(_playerTransform);
+            }
+        }
+    }
+}
