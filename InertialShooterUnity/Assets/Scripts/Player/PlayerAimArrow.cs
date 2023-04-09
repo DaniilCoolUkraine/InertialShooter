313c1ba2fe5c71e033876cc8dafa84dc65d3fe4d
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-04-09 15:48:15 +0300

    added basic player shooting

---
+++ InertialShooterUnity/Assets/Scripts/Player/PlayerAimArrow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace InertialShooter.Player
+{
+    public class PlayerAimArrow : MonoBehaviour
+    {
+        private Camera _mainCamera;
+
+        private Vector3 _mousePosition;
+        private Vector3 _aimDirection;
+        
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
+        
+        private void Update()
+        {
+            _mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            _aimDirection = (_mousePosition - transform.position).normalized;
+
+            float angle = Mathf.Atan2(_aimDirection.x, _aimDirection.y) * Mathf.Rad2Deg;
+
+            transform.eulerAngles = new Vector3(0, 0, -angle);
+        }
+    }
+}
---
+++ InertialShooterUnity/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace InertialShooter.Player
+{
+    public class PlayerShoot : MonoBehaviour
+    {
+        public event Action OnShoot;
+
+        [SerializeField] private Rigidbody2D _rb;
+        
+        [SerializeField] private float _cooldown;
+        [SerializeField] private float _recoil;
+        
+        private bool _canShoot = true;
+
+        private Camera _mainCamera;
+
+        private Vector2 _mouseClickPosition;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Fire();
+            }
+        }
+
+        private void Fire()
+        {
+            if (_canShoot)
+            {
+                _mouseClickPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                _rb.AddRelativeForce(-1 * _mouseClickPosition.normalized * _recoil);
+
+                StartCoroutine(ShootCooldown());
+            }
+        }
+        
+        private IEnumerator ShootCooldown()
+        {
+            _canShoot = false;
+            yield return new WaitForSeconds(_cooldown);
+            _canShoot = true;
+        }
+    }
+}
