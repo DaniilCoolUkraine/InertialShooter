9dd5b351043a1668211f8ad4925bdf219b6fa7fb
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-04-13 22:14:37 +0300

    updated player dash, added post-processing

---
+++ InertialShooterUnity/Assets/Scripts/General/HelperFunctions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace InertialShooter.General
+{
+    public class HelperFunctions
+    {
+        public static Vector3 VectorAbs(Vector3 vector)
+        {
+            float x = Mathf.Abs(vector.x);
+            float y = Mathf.Abs(vector.y);
+            float z = Mathf.Abs(vector.z);
+
+            return new Vector3(x, y, z);
+        }
+
+        public static Vector3 VectorDivision(Vector3 firstVector, Vector3 secondVector)
+        {
+            float x = firstVector.x / secondVector.x;
+            float y = firstVector.y / secondVector.y;
+            float z = firstVector.z / secondVector.z;
+            
+            return new Vector3(x, y, z);
+        }
+    }
+}
--- InertialShooterUnity/Assets/Scripts/Player/PlayerDash.cs
+++ InertialShooterUnity/Assets/Scripts/Player/PlayerDash.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using DG.Tweening;
+using InertialShooter.General;
+using UnityEngine;
 
 namespace InertialShooter.Player
 {
@@ -6,11 +9,28 @@ namespace InertialShooter.Player
     {
         [SerializeField] private Rigidbody2D _rb;
         [SerializeField] private float _recoil;
+
+        [SerializeField] private float _duration;
+        private Vector3 _startScale;
         
         public void Dash(Vector2 direction)
         {
             _rb.AddForce(direction * _recoil, ForceMode2D.Impulse);
-            _rb.AddTorque(Random.Range(10, 40));
+
+            StartCoroutine(Strech(direction));
+        }
+
+        private IEnumerator Strech(Vector2 direction)
+        {
+            _startScale = transform.localScale;
+            Vector3 absoluteDirection = HelperFunctions.VectorAbs(direction);
+
+            Vector3 dashScale =
+                HelperFunctions.VectorDivision(absoluteDirection, absoluteDirection + new Vector3(0.03f, 0.03f, 0.03f)).normalized;
+            
+            transform.DOScale(dashScale, _duration);
+            yield return new WaitForSeconds(_duration);
+            transform.DOScale(_startScale, _duration);
         }
     }
 }
